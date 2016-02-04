f556fd7c5793201394e3100392057db501744bcd
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-02-04 17:27:16 -0200

    Mandar pro heroku

--- cleonice/models.py
+++ cleonice/models.py
@@ -1,2 +1,20 @@
 from django.db import models
 from django.utils import timezone
+from cloudinary.models import CloudinaryField
+
+
+class Product(models.Model):
+	product_name = models.CharField('Nome', max_length=200)
+	description = models.TextField('Descrição')
+	price = models.DecimalField(decimal_places=2, max_digits=18)
+	published_date = models.DateTimeField(auto_now_add=True)
+
+	def __str__ (self):
+		return self.description
+
+class ProductImage(models.Model):
+	product = models.ForeignKey(Product, on_delete=models.CASCADE)
+	image = CloudinaryField('imagem', blank=True, null=True)
+	description = models.CharField('Descrição', max_length=200)
+
+
