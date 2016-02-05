1b875409465dec0b3c79bc8bcb3de96d3649979c
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-02-04 23:03:00 -0200

    Add product type

--- cleonice/models.py
+++ cleonice/models.py
@@ -4,10 +4,16 @@ from cloudinary.models import CloudinaryField
 
 
 class Product(models.Model):
+	PRODUCT_TYPE = (
+		('BO', 'Bolo'),
+		('BI', 'Biscoito'),
+	)
+
 	product_name = models.CharField('Nome', max_length=200)
 	description = models.TextField('Descrição')
 	price = models.DecimalField(decimal_places=2, max_digits=10)
 	published_date = models.DateTimeField(auto_now_add=True)
+	product_type = models.CharField(max_length=1, choices=PRODUCT_TYPE)
 
 	def __str__ (self):
 		return self.description
