77ee66f2cd864a7b7abd2c69b289b96748702822
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-02-08 17:26:40 -0200

    change config database_url change 200

--- cleonice/models.py
+++ cleonice/models.py
@@ -17,6 +17,7 @@ class Product(models.Model):
 
 	def __str__ (self):
 		return self.description
+		
 
 class ProductImage(models.Model):
 	product = models.ForeignKey(Product, on_delete=models.CASCADE)
--- mysite/settings.py
+++ mysite/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
 }
 
 # Update database configuration with $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config(conn_max_age=300)
 DATABASES['default'].update(db_from_env)
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
