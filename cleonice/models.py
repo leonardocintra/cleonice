b1451093e0625c2998ee21c86c230a498fe27be0
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-02-08 17:29:36 -0200

    Remove ProductImage model for test

--- cleonice/models.py
+++ cleonice/models.py
@@ -17,8 +17,9 @@ class Product(models.Model):
 
 	def __str__ (self):
 		return self.description
-		
 
+
+"""
 class ProductImage(models.Model):
 	product = models.ForeignKey(Product, on_delete=models.CASCADE)
 	image = CloudinaryField('imagem', blank=True, null=True)
@@ -26,3 +27,5 @@ class ProductImage(models.Model):
 
 	def __str__ (self):
 		return self.product.product_name
+
+"""
