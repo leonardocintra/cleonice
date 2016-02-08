808d087386b18cb1c3df767b0317e364944ede5f
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-02-08 18:51:28 -0200

    Limit field

--- cleonice/models.py
+++ cleonice/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 #from django.utils import timezone
-from cloudinary.models import CloudinaryField
+#from cloudinary.models import CloudinaryField
 
 
 class Product(models.Model):
@@ -9,23 +9,7 @@ class Product(models.Model):
 		('BI', 'Biscoito'),
 	)
 
-	name = models.CharField(max_length=200, null=False)
-	#description = models.TextField('Descrição')
-	#price = models.DecimalField(decimal_places=2, max_digits=10)
-	#published_date = models.DateTimeField(auto_now_add=True)
-	#product_type = models.CharField(max_length=2, choices=PRODUCT_TYPE, default='BO')
+	name = models.CharField(max_length=100, null=False)
 
 	def __str__ (self):
 		return self.name
-
-
-"""
-class ProductImage(models.Model):
-	product = models.ForeignKey(Product, on_delete=models.CASCADE)
-	image = CloudinaryField('imagem', blank=True, null=True)
-	description = models.CharField('Descrição', max_length=200, null=True)
-
-	def __str__ (self):
-		return self.product.name
-
-"""
