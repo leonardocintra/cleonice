3cf20e46a18e371b0e60476ca0d9e2ed2d190742
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-02-08 19:13:28 -0200

    Add model Product

--- cleonice/models.py
+++ cleonice/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-#from django.utils import timezone
+from django.utils import timezone
 #from cloudinary.models import CloudinaryField
+
+
+class Product(models.Model):
+	PRODUCT_TYPE = (
+		('BO', 'Bolo'),
+		('BI', 'Biscoito'),
+	)
+
+	product_name = models.CharField('Nome', max_length=200)
+	description = models.TextField('Descrição')
+	price = models.DecimalField(decimal_places=2, max_digits=10)
+	published_date = models.DateTimeField(auto_now_add=True)
+	product_type = models.CharField(max_length=2, choices=PRODUCT_TYPE, default='BO')
+
+	def __str__ (self):
+		return self.product_name
