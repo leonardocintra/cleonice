a9b0fbe3d9ba71c089e0a61e5221a88d26635590
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-02-08 18:59:42 -0200

    remove all models

--- cleonice/models.py
+++ cleonice/models.py
@@ -1,15 +1,3 @@
 from django.db import models
 #from django.utils import timezone
 #from cloudinary.models import CloudinaryField
-
-
-class Product(models.Model):
-	PRODUCT_TYPE = (
-		('BO', 'Bolo'),
-		('BI', 'Biscoito'),
-	)
-
-	name = models.CharField()
-
-	def __str__ (self):
-		return self.name
