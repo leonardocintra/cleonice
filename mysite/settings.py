4f2f9c9b98ca5d8c64aed99b276a2288c534931f
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-02-16 23:18:34 -0200

    changing email

--- mysite/settings.py
+++ mysite/settings.py
@@ -91,8 +91,8 @@ CLOUDINARY = {
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'emaildevleonardo@gmail.com'
-EMAIL_HOST_PASSWORD = 'julianarosarodrigues'
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
