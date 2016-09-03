434a16bea9af3eb6e89ab034d2433bc5858ab140
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-09-03 11:52:07 -0300

    add tests in contact form

--- cleonice/tests/test_views.py
+++ cleonice/tests/test_views.py
@@ -1,5 +1,7 @@
 from django.test import TestCase, Client
 from django.core.urlresolvers import reverse
+from django.core import mail
+
 
 class IndexViewTestCase(TestCase):
     def setUp(self):
@@ -16,4 +18,35 @@ class IndexViewTestCase(TestCase):
     def test_status_code(self):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
+
+
+class ContactViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('contact')
+    
+    def tearDown(self):
+        pass
+
+    def test_status_code(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_template_used(self):
+        response = self.client.get(self.url)
+        self.assertTemplateUsed(response, 'contato.html')
+    
+    # def test_form(self):
+    #     data = {'name': '', 'message': '', 'email': ''}
+    #     response = self.client.post(self.url, data)
+    #     self.assertFormError(response, 'form', 'name', 'Este campo é obrigatório.')
+    #     self.assertFormError(response, 'form', 'email', 'Este campo é obrigatório.')
+    #     self.assertFormError(response, 'form', 'message', 'Este campo é obrigatório.')
+
+    # def test_form_ok(self):
+    #     data = {'name': 'test', 'message': 'test', 'email': 'test@teste.com'}
+    #     response = self.client.post(self.url, data)
+    #     self.assertTrue(response.context['success'])
+    #     self.assertEqual(len(mail.outbox), 1) # outbox = caixa de saida 
+    #     self.assertEqual(mail.outbox[0].subject, 'Contato do Django Ecommerce')
     
--- cleonice/urls.py
+++ cleonice/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.index, name='home'),
 	url(r'^sobre/', views.about), 
-	url(r'^contato/', views.contact), 
+	url(r'^contato/', views.contact, name='contact'), 
 	url(r'^bolos/', views.cakes),
 	url(r'^produto/(?P<pk>[0-9]+)/$', views.product, name='product'),
 ]
