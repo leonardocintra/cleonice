6ddce668364f8105c6bf8f18b4b2b5fbff7b3689
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-09-03 11:21:01 -0300

    fazendo testes para vincular com o travis

--- cleonice/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-from .models import Product
-
-class ProductTest(TestCase):
-	def setUp(self):
-		Product.objects.create(product_name="Bolo de Cenoura")
-		Product.objects.create(product_name="Bolo de Fuba")
-		Product.objects.create(product_name="Bolo de Morango")
