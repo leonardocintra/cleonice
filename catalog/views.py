faf251d9dfe47d8db691d4303ad21bde1cb419ba
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-12-19 13:21:53 -0200

    cadatro categoria ok

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
 
-# Create your views here.
+from .models import Product, Category
+
+
+class ProductListView(generic.ListView):
+    model = Product
+    template_name = 'catalog/product_list.html'
+    context_object_name = 'product_list'
+    paginate_by = 10
