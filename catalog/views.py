56d47f21dc4a45cdf9c6e9eedf6a88c0d21affc7
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-12-21 12:55:12 -0200

    acertado as url

--- catalog/views.py
+++ catalog/views.py
@@ -9,3 +9,21 @@ class ProductListView(generic.ListView):
     template_name = 'catalog/product_list.html'
     context_object_name = 'product_list'
     paginate_by = 10
+
+
+class CategoryListView(generic.ListView):
+    template_name = 'catalog/category.html'
+    context_object_name = 'product_list'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return Product.objects.filter(category__slug=self.kwargs['slug'])
+
+    def get_context_data(self, **kwargs):
+        context = super(CategoryListView, self).get_context_data(**kwargs)
+        context['current_category'] = get_object_or_404(Category, slug=self.kwargs['slug'])
+        return context
+
+
+product_list = ProductListView.as_view()
+category = CategoryListView.as_view()
--- cleonice/urls.py
+++ cleonice/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from core import views
 
 urlpatterns = [
-    url(r'', include('core.urls')),
+    url(r'^$', views.index, name='index'),
+	url(r'^sobre/', views.about), 
+	url(r'^contato/', views.contact, name='contact'), 
+	url(r'^catalogo/', include('catalog.urls', namespace='catalog')),
     url(r'^admin/', admin.site.urls),
+	#url(r'^bolos/', views.cakes, name='cakes'),
+	#url(r'^produto/(?P<pk>[0-9]+)/$', views.product, name='product'),
 ]
