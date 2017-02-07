80e73bd7bc5c1b8e222f8d448026a2b6322ade36
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-02-07 16:28:52 -0200

    mini refazimento do index

--- core/forms.py
+++ core/forms.py
@@ -2,7 +2,9 @@ from django import forms
 
 
 class FormContact(forms.Form):
-	name = forms.CharField(label='Nome', widget=forms.TextInput(attrs={'class': 'form-control'}))
-	email = forms.EmailField(label='Email', widget=forms.TextInput(attrs={'class': 'form-control'}))
-	phone = forms.CharField(label='Telefone', max_length=13, widget=forms.TextInput(attrs={'class': 'form-control'}))
-	message = forms.CharField(label='Mensagem', widget=forms.Textarea(attrs={'class': 'form-control'}), max_length=500 )
+	""" Gera o formulario de contato"""
+	
+	name = forms.CharField(label='Nome', widget=forms.TextInput())
+	email = forms.EmailField(label='Email', widget=forms.TextInput())
+	phone = forms.CharField(label='Telefone', max_length=13, widget=forms.TextInput())
+	message = forms.CharField(label='Mensagem', widget=forms.Textarea(), max_length=500)
